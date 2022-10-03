9f1e431018111dbee2a33cf83dc04176ead3f81a
Stephen Michaels <stephen.michaels3333@gmail.com>
2022-10-02 20:00:38 -0400

    initial commit

---
+++ playingwithvariablesConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Variable Declarations and Initializations
+int dogAge = 4;
+string dogName = "Lilly";
+bool likesWalks = true;
+char nickName = 'L';
+double tennisBalls = 33;
+decimal weight = 60.5m;
+
+Console.WriteLine($"My dog's name is {dogName}, She is {dogAge} years old.  She loves to chase her {tennisBalls} tennis balls, which keeps her at {weight} Lbs.  It is also {likesWalks} that she likes walks.");
